3c5bb72797f0a9ff9b945468c36fa95accce87dc
Aarti Nanwani Samtani <aarti@MacBook-Pro-de-Aarti.local>
2025-09-15 18:05:03 +0200

    first commit

--- 02Lenguajes/3-Ejercicio/src/main.js
+++ 02Lenguajes/3-Ejercicio/src/main.js
@@ -1,3 +1,22 @@
-import "./style.css";
+//Implementa una función clone que, a partir de un objeto de entrada source devuelva un nuevo objeto con las propiedades de source:
+function clone(source) {
+  return { ...source };
+}
 
-console.log("Hello from main");
+const original = { a: 1, b: 2, c: 3 };
+const copy = clone(original);
+
+console.log(original);
+console.log(copy);
+
+//Implementa una función merge que, dados dos objetos de entrada source y target, devuelva un nuevo objeto con todas las propiedades de target y de source, y en caso de propiedades con el mismo nombre, source sobreescribe a target.
+function merge(source, target) {
+  return { ...source, ...target };
+}
+
+var a = { name: "Maria", surname: "Ibañez", country: "SPA" };
+var b = { name: "Luisa", age: 31, married: true };
+
+const result = merge(b, a);
+
+console.log(result);
