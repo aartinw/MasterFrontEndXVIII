b92e48a499e2a07d1bc9cf290c97b790734d062a
Aarti Nanwani Samtani <aarti@MacBook-Pro-de-Aarti.local>
2025-09-15 17:49:52 +0200

    first commit

--- 02Lenguajes/2-Ejercicio/src/main.js
+++ 02Lenguajes/2-Ejercicio/src/main.js
@@ -1,3 +1,16 @@
-import "./style.css";
+//Implementa una función concat (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators.
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
 
-console.log("Hello from main");
+const concat = (a, b) => {
+  return [...a, ...b];
+};
+
+console.log(concat(array1, array2));
+
+//Opcional
+const array3 = [7, "Gran Canaria", 30, true];
+const array4 = [8, 10, "adios"];
+
+const concatMulti = (...arrays) => [].concat(...arrays);
+console.log(concatMulti(array1, array2, array3, array4));
