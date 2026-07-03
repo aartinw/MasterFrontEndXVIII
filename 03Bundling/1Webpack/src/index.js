afe7538aa12bc6d6c66695208528f5bfbe347da2
Aarti <aarti.95@hotmail.com>
2026-07-03 19:49:00 +0200

    entrega webpack

--- 03Bundling/1Webpack/src/index.js
+++
@@ -1,6 +0,0 @@
-import "./mystyles.scss";
-import logoImg from "./images/logo_1.png";
-
-const img = document.createElement("img");
-img.src = logoImg;
-document.getElementById("imgContainer").appendChild(img);
